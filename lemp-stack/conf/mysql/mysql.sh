1b6395b4c87acef9f249003932364090a55e599f
John Lago <750845+Lagoja@users.noreply.github.com>
2022-10-14 11:28:45 -0700

    cleanup mysql.sh comments

--- lemp-stack/conf/mysql/mysql.sh
+++ lemp-stack/conf/mysql/mysql.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
     
     set -eux
-    # export MYSQL_BASEDIR=$(which mariadb | sed -r "s/\/bin\/mariadb//g")
-    # export MYSQL_HOME=$PWD/conf/mysql
-    # export MYSQL_DATADIR=$MYSQL_HOME/data
-    # export MYSQL_UNIX_PORT=$MYSQL_HOME/mysql.sock
-    # export MYSQL_PID_FILE=$MYSQL_HOME/mysql.pid
     alias mysql='mysql -u root'
 
     if [ ! -d "$MYSQL_DATADIR" ]; then
@@ -20,10 +15,3 @@
     mysqld --datadir=$MYSQL_DATADIR --pid-file=$MYSQL_PID_FILE \
 	    --socket=$MYSQL_UNIX_PORT 2> $MYSQL_HOME/mysql.log & MYSQL_PID=$!
     
-#    finish()
-#    {
-#      mysqladmin -u root --socket=$MYSQL_UNIX_PORT shutdown
-#      kill $MYSQL_PID
-#      wait $MYSQL_PID
-#    }
-#    trap finish EXIT
