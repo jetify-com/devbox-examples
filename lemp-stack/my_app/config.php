895660c733a59a74f0a60edacd18d5a62713d879
John Lago <750845+Lagoja@users.noreply.github.com>
2022-10-17 15:40:06 -0700

    connect PHP to MariaDB in LAMP + LEMP

---
+++ lemp-stack/my_app/config.php
@@ -0,0 +1,6 @@
+<?php
+	$db_hostname	= "127.0.0.1";
+	$db_port 		= "3306";
+	$db_database	= "devbox_lamp";
+	$db_username	= "devbox_user";
+	$db_password	= "password";
---
+++ lemp-stack/setup_db.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS devbox_lamp;
+CREATE DATABASE devbox_lamp;
+
+USE devbox_lamp
+
+CREATE USER 'devbox_user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON devbox_lamp.* TO 'devbox_user'@'localhost' IDENTIFIED BY 'password';
+
+DROP TABLE IF EXISTS colors;
+CREATE TABLE colors (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL,
+	hex VARCHAR(7) NOT NULL,
+	PRIMARY KEY (id));
+
+INSERT INTO colors (name, hex) VALUES ('red', '#FF0000'), ('blue', '#0000FF'), ('green', '#00FF00');
+
+
