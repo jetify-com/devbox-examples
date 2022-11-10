38cde0267c9bac14a6573da858cf1731498e4fd2
John Lago <750845+Lagoja@users.noreply.github.com>
2022-11-10 13:52:55 -0800

    Add LEMP and Rust

--- stacks/lemp-stack/conf/set-exit.sh
+++
@@ -1,8 +0,0 @@
-    finish()
-    {
-      mysqladmin -u root --socket=$MYSQL_UNIX_PORT shutdown
-      pkill -SIGINT -F $PHP_CONFDIR/php-fpm.pid
-      pkill nginx
-      rm $PHP_CONFDIR/php-fpm.pid
-    }
-    trap finish EXIT SIGHUP
