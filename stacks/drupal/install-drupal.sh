39b1f7d7f8e2c322541a33d5037e452a7f84f180
Ofer Shaal <shaalofer@gmail.com>
2022-11-12 00:05:36 -0500

    fix: update install script

--- stacks/drupal/install-drupal.sh
+++ stacks/drupal/install-drupal.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
+echo "Make sure to run this script only after 'devbox shell'"
+
 mysql -u root < setup_db.sql
 composer install
-drush si demo_umami -y
+vendor/bin/drush si demo_umami -y
 
 echo "Your Drupal demo website is ready,"
 echo "Open localhost:8000 in your browser."
